2b7939c0870e70c590ad5d2f3d71e9aaf99322c5
Jun Wang <junw3@uw.edu>
2023-02-07 22:39:43 -0800

    segment tree

---
+++ Lib/SegmentTree.java
@@ -0,0 +1,127 @@
+// Author: MJUNM
+
+package Lib;
+
+/**
+ * For range queries with frequent updates.
+ *
+ * build() - O(n)
+ * query() - O(log n)
+ * update() - O(log n)
+ */
+public class SegmentTree {
+    // Segment Tree Nodes
+    private int[] tree;
+
+    /**
+     * Construct and fill the segment tree
+     *
+     * @param arr input array for building
+     */
+    SegmentTree(int[] arr) {
+        // Number of elements in the tree
+        int n = arr.length;
+
+        // Height of the tree
+        int h = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+
+        // Size of the array for storing the tree
+        int size = 2 * (int) Math.pow(2, h) - 1;
+        tree = new int[size];
+
+        // Recursively build the tree
+        buildHelper(arr, 0, n - 1, 0);
+    }
+
+    // helper function for building the tree
+    private int buildHelper(int[] arr, int start, int end, int curr) {
+        // At leaf node -> store the value
+        if (start == end) {
+            tree[curr] = arr[start];
+            return arr[start];
+        }
+
+        // Not at leaf node -> Recurrence for left and right subtrees
+        int mid = start + (end - start) / 2;
+        tree[curr] = buildHelper(arr, start, mid, curr * 2 + 1) +
+                buildHelper(arr, mid + 1, end, curr * 2 + 2);
+        return tree[curr];
+    }
+
+    /**
+     * Update the given index of the array with the new value.
+     * Update segment tree values as appropriate.
+     *
+     * @param arr    array for replacing the value
+     * @param i      index to replace in the array
+     * @param newVal value for replacement
+     */
+    void update(int[] arr, int i, int newVal) {
+        int n = arr.length;
+
+        // Invalid index input
+        if (i < 0 || i > n - 1) {
+            throw new IllegalArgumentException("Invalid index input!");
+        }
+
+        // Update array and tree
+        int diff = newVal - arr[i];
+        arr[i] = newVal;
+        updateHelper(0, n - 1, i, diff, 0);
+    }
+
+    // helper function for updating the values in the tree
+    private void updateHelper(int start, int end, int i, int diff, int curr) {
+        // base case: Range is outside the segment
+        if (i < start || i > end) return;
+
+        tree[curr] = tree[curr] + diff;
+        if (start != end) {
+            int mid = start + (end - start) / 2;
+            updateHelper(start, mid, i, diff, curr * 2 + 1);
+            updateHelper(mid + 1, end, i, diff, curr * 2 + 2);
+        }
+    }
+
+    /**
+     * Query the sum of a given range in the tree
+     *
+     * @param n     number of elements
+     * @param start start of the range
+     * @param end   end of the range
+     * @return the sum of the given range
+     */
+    int getSum(int n, int start, int end) {
+        if (start < 0 || end > n - 1 || start > end) {
+            throw new IllegalArgumentException();
+        }
+
+        return getSumHelper(0, n - 1, start, end, 0);
+    }
+
+    // helper function for querying the sum of a given range in the tree
+    private int getSumHelper(int start, int end, int queryStart, int queryEnd, int curr) {
+        // Segment is part of given range -> return the sum of the segment
+        if (queryStart <= start && queryEnd >= end) return tree[curr];
+
+        // Segment is outside the given range
+        if (end < queryStart || start > queryEnd) return 0;
+
+        // Segment overlaps with the given range
+        int mid = start + (end - start) / 2;
+        return getSumHelper(start, mid, queryStart, queryEnd, curr * 2 + 1) +
+                getSumHelper(mid + 1, end, queryStart, queryEnd, curr * 2 + 2);
+    }
+
+
+    // Tests
+    public static void main(String[] args) {
+        int[] arr = {-4, -1, 0, 4, 1, 0, 9, 18};
+        int n = arr.length;
+        SegmentTree st = new SegmentTree(arr);
+        System.out.println("Sum of values from index 1 to 5 = " + st.getSum(n, 1, 5));
+        System.out.println("Update: set arr[2] = 11");
+        st.update(arr, 2, 11);
+        System.out.println("After update: Sum of values from index 1 to 5 = " + st.getSum(n, 1, 5));
+    }
+}
---
+++ Solve.java
@@ -0,0 +1,84 @@
+// Author: MJUNM
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Solve {
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        // Start Coding
+
+        out.close();
+    }
+
+    /* ----- Pre-defined Resources ----- */
+    static final int mod = 1_000_000_007;
+
+    static long add(long a, long b) {
+        return (a + b) % mod;
+    }
+
+    static long sub(long a, long b) {
+        return ((a - b) % mod + mod) % mod;
+    }
+
+    static long mul(long a, long b) {
+        return (a * b) % mod;
+    }
+
+    static long exp(long base, long exp) {
+        if (exp == 0) return 1;
+        long half = exp(base, exp / 2);
+        if (exp % 2 == 0) return mul(half, half);
+        return mul(half, mul(half, base));
+    }
+
+    static class MyScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+    }
+}
