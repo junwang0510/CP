09a810142a34c39dcea938271f1172dce2b3c3c8
Jun Wang <junw3@uw.edu>
2023-03-04 02:05:15 -0800

    cpp contest template

---
+++ Template.cpp
@@ -0,0 +1,31 @@
+// Author: MJUNM
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+// Less unnecessary content and more compatible than <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  // Solution here
+}
+
