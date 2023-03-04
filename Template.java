c165256d3106d6050d122145aa8c748f8209792b
Jun Wang <junw3@uw.edu>
2023-03-04 05:53:16 -0800

    simplified Java Template

--- Template.java
+++ Template.java
@@ -1,64 +1,40 @@
 // Author: MJUNM
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Template {
-
-    static class Task {
-        public void solve(MyScanner sc, PrintWriter out) {
-            // Solution here
-        }
-    }
-
     public static void main(String[] args) {
-        MyScanner sc = new MyScanner(System.in);
+        MyScanner in = new MyScanner();
         PrintWriter out = new PrintWriter(System.out);
-        Task solver = new Task();
-        solver.solve(sc, out);
+
+        // Solution Here
+
         out.close();
     }
 
     static class MyScanner {
-        public BufferedReader reader;
-        public StringTokenizer tokenizer;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
 
-        public MyScanner(InputStream stream) {
-            reader = new BufferedReader(new InputStreamReader(stream), 32768);
-            tokenizer = null;
-        }
-
-        public String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        String next() {
+            while (!st.hasMoreTokens()) {
                 try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
             }
-            return tokenizer.nextToken();
+            return st.nextToken();
         }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        public double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
         String nextLine() {
             String str = "";
             try {
-                str = reader.readLine();
+                str = br.readLine();
             } catch (IOException e) {
                 e.printStackTrace();
             }
